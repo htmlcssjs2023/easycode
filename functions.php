92aa906d10e90c77bddb67e4c841c790da99a6a2
mdabdulhalim <htmlcssjs2023>
2023-06-19 10:51:18 +0600

    Walker Menu

--- functions.php
+++ functions.php
@@ -104,3 +104,18 @@ false);
 add_action('wp_enqueue_scripts', 'easycode_add_google_fonts');
 // Menu Register
 register_nav_menu( 'main_menu', __('Main Menu', 'mdabdulhalim') );
+
+
+// Walker Menu Properties
+function ali_nav_description( $item_output, $item, $args){
+if( !empty ($item->description)){
+$item_output = str_replace($args->link_after . '</a>', '<span class="walker_nav">' . $item->description . '</span>' .
+$args->link_after . '</a>', $item_output);
+}
+return $item_output;
+}
+add_filter('walker_nav_menu_start_el', 'ali_nav_description', 10, 3);
+
+
+
+
--- index.php
+++ index.php
@@ -12,11 +12,11 @@
 </head>
 
 <body <?php body_class(); ?>>
-    <div id="header_area" class="<?php echo get_theme_mod('halim_menu_position'); ?>">
+    <header id="header_area" class="<?php echo get_theme_mod('halim_menu_position'); ?>">
         <div class="container">
             <div class="row" id="menu_and_logo_area">
                 <div class="col-md-3">
-                    <a href="#"><img src="<?php echo get_theme_mod('easycode_logo'); ?>" alt=""></a>
+                    <a href="<?php home_url(); ?>"><img src="<?php echo get_theme_mod('easycode_logo'); ?>" alt=""></a>
                 </div>
                 <div class="col-md-9">
                         
@@ -24,7 +24,17 @@
                 </div>
             </div>
         </div>
-    </div>
+    </header>
+
+    <section class="" id="body_area">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <?php the_content() ?>
+                </div>
+            </div>
+        </div>
+    </section>
 
 <?php wp_footer();?>
 </body>
